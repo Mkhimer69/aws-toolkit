a4699fc54cf81d3335e9b1e5e05ae21094668a3c
Fathy Mkhimer <horriaalex2012@gmail.com>
2026-08-29 22:03:06 +0300

    Update AWS-Toolkit.Beta.user.js

--- AWS-Toolkit.Beta.user.js
+++ AWS-Toolkit.Beta.user.js
@@ -5,6 +5,7 @@
 // @description  A productivity toolkit built for Amazon Connect user administration workflows.
 // @author       Fathy Mkhimer
 // @match        https://lyft-support.my.connect.aws/users*
+// @match        https://lyft-support.my.connect.aws/analytics-dashboards*
 // @downloadURL  https://raw.githubusercontent.com/Mkhimer69/aws-toolkit/main/AWS-Toolkit.user.js
 // @updateURL    https://raw.githubusercontent.com/Mkhimer69/aws-toolkit/main/AWS-Toolkit.user.js
 // @grant        none
