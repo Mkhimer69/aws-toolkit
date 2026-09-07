91537d8159e9b680b23f64658c8294ecc4d5e4f1
Fathy Mkhimer <horriaalex2012@gmail.com>
2026-09-07 05:16:46 +0300

    Update AWS-Toolkit.user.js

--- AWS-Toolkit.user.js
+++ AWS-Toolkit.user.js
@@ -8,6 +8,7 @@
 // @match        https://lyft-support.my.connect.aws/analytics-dashboards*
 // @downloadURL  https://raw.githubusercontent.com/Mkhimer69/aws-toolkit/main/AWS-Toolkit.user.js
 // @updateURL    https://raw.githubusercontent.com/Mkhimer69/aws-toolkit/main/AWS-Toolkit.user.js
+// @icon         https://i.imgur.com/cYXhTph.png
 // @grant        none
 // ==/UserScript==
 
